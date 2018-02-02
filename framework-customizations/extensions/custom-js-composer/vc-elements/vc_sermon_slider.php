442940850528a8371dd3810ca4e213085f281724
AnhPhi <nguyenanhphi077@gmail.com>
2018-02-02 14:49:00 +0700

    Update small style and element sermon, event :rocket:

--- framework-customizations/extensions/custom-js-composer/vc-elements/vc_sermon_slider.php
+++ framework-customizations/extensions/custom-js-composer/vc-elements/vc_sermon_slider.php
@@ -172,12 +172,11 @@ $rand_id = rand(9, 99999);
 	<div class="modal fade" id="<?php echo sprintf('sm-modal-media-%s', $rand_id); ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" data-semon-modal>
 	  <div class="modal-dialog" role="document">
 		<div class="modal-content">
-		  
+		  <div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal">&times;</button>
+		  </div>
 		  <div class="modal-body" data-semon-modal-content>
 			Loading...
-		  </div>
-		  <div class="modal-footer">
-			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 		  </div>
 		</div>
 	  </div>
